3454c7cc516b23e3562385850d71b624b150fc64
Your Name <you@example.com>
2021-03-29 10:56:36 +0700

    sciprt

---
+++ MyScript/preprocess.sh
@@ -0,0 +1,3 @@
+PROCESSED_DIR=500k_roberta
+VOCAB_DIR=PhoBERT_base_fairseq
+fairseq-preprocess --source-lang src --target-lang trg --trainpref $PROCESSED_DIR/train --validpref $PROCESSED_DIR/valid --testpref $PROCESSED_DIR/test --destdir $PROCESSED_DIR/bin --srcdict $VOCAB_DIR/dict.txt --tgtdict $VOCAB_DIR/dict.txt
